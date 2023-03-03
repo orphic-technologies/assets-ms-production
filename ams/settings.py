c38823ea6b693c4858ad6ee622e20d6cbedc3523
Aaditya Dulal <artdityadulal@gmail.com>
2023-03-03 19:58:57 +0545

    Production Database Settings Update

--- ams/settings.py
+++ ams/settings.py
@@ -77,8 +77,13 @@ WSGI_APPLICATION = "ams.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "asset_db",
+        "USER": "asset_user",
+        "PASSWORD": "SWN@NEPAL#444",
+        "HOST": "localhost",
+        "PORT": "",
+
     }
 }
 
